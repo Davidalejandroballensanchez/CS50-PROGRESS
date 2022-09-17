cae4617d30944aa1c6c5fc3db63f1d8c140108fe
David Alejandro Ballen Sanchez <aballenplays@gmail.com>
2022-09-16 19:28:52 -0500

    Continuamos desarrollando y mejorando nuestras habilidades con el compilador de C y con funciones, y condicionales.

--- agree/agree.c
+++ agree/agree.c
@@ -1,8 +1,21 @@
 #include "cs50.h"
 #include <stdio.h>
 
+// Importante recordar que para (char) usamos ('') por ser char un tipo de dato unico caracter.
+// Importante recordar que para (string) usamos ("") por ser string un tipo de dato de varios caractertes. 
+
 int main(void)
 {
-    // Hola alejo soy tu yo del pasado para recordarte que cuando entres a este archivo 
-    // termines de copiarlo y compilarlo y ejecutarlo XDDDD
+    // Pregunta al usuario Si Concuerda
+    char c = get_char("Do you agree? ");
+
+    // Comprobar al usuario Si Concuerda
+    if (c == 'y' || c == 'Y')
+    {
+        printf("Agreed.\n");
+    }
+    else if (c == 'n' || c == 'N')
+    {
+        printf("Not Agreed.\n");
+    }
 }
